7bdb521fa36682fc98344923b3de1a103f307d98
KuPoK <k>
2022-12-27 12:12:01 +0500

    added buttons, input boxes in secondActivity, fixed bug with save button

--- app/src/main/java/com/example/myday/AddTaskActivity.kt
+++ app/src/main/java/com/example/myday/AddTaskActivity.kt
@@ -2,10 +2,15 @@ package com.example.myday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AddTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
+    }
+
+    fun GoToMainActivity(view : View) {
+        finish()
     }
 }
