39da3aa52d6ef763eb43fb464a7f28009e0bf748
Kirill <kirill_koksharov@list.ru>
2023-01-06 15:38:32 +0500

    added date under logo on mainActivity, added launcher

--- app/src/main/java/com/example/myday/AddTaskActivity.kt
+++ app/src/main/java/com/example/myday/AddTaskActivity.kt
@@ -1,14 +1,14 @@
 package com.example.myday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.myday.databinding.ActivityAddTaskBinding
-import com.example.myday.databinding.ActivityMainBinding
 
 class AddTaskActivity : AppCompatActivity() {
-
     lateinit var binding : ActivityAddTaskBinding
+    private var category: String = "1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +16,32 @@ class AddTaskActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    fun GoToMainActivity(view : View) {
+    fun setCategoryDefault(view: View) {
+        category = "1"
+    }
+
+    fun setCategoryCalendar(view: View) {
+        category = "2"
+    }
+
+    fun setCategoryChill(view: View) {
+        category = "3"
+    }
+
+    fun goToMainActivity(view : View) {
+        val i = Intent()
+        i.putExtra("title", binding.getTitle.text.toString())
+        i.putExtra("time", binding.getTime.text.toString())
+        i.putExtra("date", binding.getDate.text.toString())
+        i.putExtra("category", category)
+        i.putExtra("description", binding.getDescription.text.toString())
+        setResult(RESULT_OK, i)
         finish()
     }
+
+    fun closeAddTaskActivity(view: View) {
+        setResult(RESULT_CANCELED)
+        finish()
+    }
+
 }
--- app/src/main/java/com/example/myday/MainActivity.kt
+++ app/src/main/java/com/example/myday/MainActivity.kt
@@ -1,28 +1,68 @@
 package com.example.myday
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myday.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var launcher : ActivityResultLauncher<Intent>? = null
     lateinit var binding : ActivityMainBinding
+
+    private var title : String? = null
+    private var time : String? = null
+    private var date : String? = null
+    private var category : String? = null
+    private var description : String? = null
+    //private var currentDate : Date = Calendar.getInstance().time
+    var currentDate: String = SimpleDateFormat("dd MMMM, yyyy", Locale.getDefault()).format(Date())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.CurrentDate.text = currentDate
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                result : ActivityResult ->
+            if (result.resultCode == RESULT_OK) {
+
+                title = result.data?.getStringExtra("title")
+                time = result.data?.getStringExtra("time")
+                date = result.data?.getStringExtra("date")
+                category = result.data?.getStringExtra("category")
+                description = result.data?.getStringExtra("description")
+
+                binding.ExampleTitle.text = title
+
+                if (time.toString().isEmpty()) { binding.ExampleTime.visibility = View.GONE }
+                else {
+                    binding.ExampleTime.visibility = View.VISIBLE
+                    binding.ExampleTime.text = time
+                }
+
+                if (category == null) binding.ExampleTaskBoxCategory.setImageResource(R.drawable.default_category)
+                else if (category == "1") binding.ExampleTaskBoxCategory.setImageResource(R.drawable.default_category)
+                else if (category == "2") binding.ExampleTaskBoxCategory.setImageResource(R.drawable.calendar_category)
+                else if (category == "3") binding.ExampleTaskBoxCategory.setImageResource(R.drawable.chill_category)
+            }
+        }
+
     }
 
     fun GoToAddTaskActivity(view : View) {
-        var i = Intent(this, AddTaskActivity::class.java)
-        startActivity(i)
+        launcher?.launch(Intent(this, AddTaskActivity::class.java))
     }
 
     fun GoToSettingsActivity(view : View) {
         var i = Intent(this, SettingsActivity::class.java)
         startActivity(i)
     }
+
 }
